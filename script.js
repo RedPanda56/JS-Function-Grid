ef7392086d123e61666a809fc032cecf43aaf089
Garrett Michalke <garrett.michalke@gmail.com>
2017-01-18 23:33:14 +0000

    Flag Updated

--- script.js
+++ script.js
@@ -14,14 +14,40 @@ setColor(0, 9, "white")
 
 setColor(1, 0, "blue")
 setColor(1, 2, "blue")
-setColor(1, 0, "blue")
+setColor(1, 3, "white")
 setColor(1, 4, "blue")
-setColor(1, 0, "blue")
+setColor(1, 5, "white")
 setColor(1, 6, "red")
 setColor(1, 7, "red")
 setColor(1, 8, "red")
 setColor(1, 9, "red")
+
+setColor(2, 0, "white")
 setColor(2, 1, "blue")
-setColor(1, 0, "blue")
-setColor(1, 0, "blue")
-setColor(1, 0, "blue")
+setColor(2, 2, "white")
+setColor(2, 3, "blue")
+setColor(2, 4, "white")
+setColor(2, 5, "blue")
+setColor(2, 6, "white")
+setColor(2, 7, "white")
+setColor(2, 8, "white")
+setColor(2, 9, "white")
+
+setColor(3, 0, "blue")
+setColor(3, 1, "white")
+setColor(3, 2, "blue")
+setColor(3, 3, "white")
+setColor(3, 4, "blue")
+setColor(3, 5, "white")
+setColor(3, 6, "red")
+setColor(3, 7, "red")
+setColor(3, 8, "red")
+setColor(3, 9, "red")
+
+
+
+
+
+
+
+
